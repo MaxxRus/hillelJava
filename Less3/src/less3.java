4e382073a2a796cfde1aed2ac90c2075e37aea86
l.gaidanov <bbh.of.odessa@gmail.com>
2015-11-05 20:38:42 +0200

    cover

---
+++ Less3/src/less3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * Created by ITHILLEL3 on 05.11.2015.
+ */
+public class less3 {
+
+
+    public static void main(String[] args) {
+        double res = add(2,3);
+       // System.out.println(res);
+        double res1 =add(2.2, 3.2);
+       // System.out.println(res1);
+        int i =10;
+        int[] arr;
+        int[] arr1;
+        arr = new int[i];
+        System.out.println(arr[1]);
+        arr[0] = 100;
+        System.out.println(arr[0]);
+        arr1 = new int[]{1,2,3};
+
+        int[] test new int[20];
+        arr.length
+
+
+
+
+
+    }
+
+    public static double add (int first, int second) {
+        System.out.println("integer");
+        double resalt = first + second;
+        return resalt;
+    }
+
+    public static double add (double first, double second) {
+        System.out.println("double");
+        double resalt = first + second;
+        return resalt;
+    }
+
+}
---
+++ cover/src/cover.java
@@ -0,0 +1,13 @@
+/**
+ * Created by ITHILLEL3 on 05.11.2015.
+ */
+public class cover {
+
+    for (int i = 0; i< 10; i++){
+        for (int j = 0; j<12;j++){
+            if (i%2==0 && j%2==0){
+                System.out.println("*");
+            }
+        }
+    }
+}
