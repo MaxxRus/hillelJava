34aff9fc35c22d208f2727d6ab267a54d13b68bf
MaxxRus <rus.maxx@gmail.com>
2015-11-07 00:49:50 +0200

    work correct if height eq width

--- cover/src/cover.java
+++ cover/src/cover.java
@@ -1,13 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Created by ITHILLEL3 on 05.11.2015.
  */
 public class cover {
+    public static void main(String[] args) {
+        System.out.println("Welcome draw cover");
+        int a = 0;
+        int b = 0;
 
-    for (int i = 0; i< 10; i++){
-        for (int j = 0; j<12;j++){
-            if (i%2==0 && j%2==0){
-                System.out.println("*");
-            }
+        System.out.println("Input height: ");
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+            a = scanner.nextInt();
+        } catch (InputMismatchException ex) {
+            System.out.println("Input wrong");
         }
+
+        System.out.println("Input widtht: ");
+
+        try {
+            b = scanner.nextInt();
+        } catch (InputMismatchException ex) {
+            System.out.println("Input wrong");
+        }
+        scanner.close();
+
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                if ((i == 0) || (i == a - 1) || (j == 0) || (j == b - 1) || (i==j) ||(i==b-1-j)) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.print('\n');
+        }
+
     }
-}
+
+
+    }
