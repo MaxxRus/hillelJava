902b2031de1017cd1bbd4cadc2fdb676156241bc
l.gaidanov <bbh.of.odessa@gmail.com>
2015-11-09 19:42:22 +0200

    test arr in

--- Less3/src/less4.java
+++ Less3/src/less4.java
@@ -4,15 +4,24 @@
 public class less4 {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5};
-        int res = sum(arr);
-        System.out.println(res);
+        int l = 5;
+        int res = sum(arr,l);
+        System.out.println(l);
+
+        for (int el:arr){
+            System.out.println(el);
+        }
     }
 
-    public static int sum(int[] arr) {
+    public static int sum(int[] arr, int length) {
         int sum = 0;
-        for (int el : arr){
-            sum +=el;
+        for (int i = 0; i < length; i++) {
+            sum +=arr[i];
         }
+
+        arr[0] = sum;
+        length = 10;
         return sum;
+
     }
 }
