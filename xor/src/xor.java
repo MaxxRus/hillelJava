f052ebc650c780c106634475eebee2fd7a0a9465
MaxxRus <rus.maxx@gmail.com>
2015-11-18 12:43:56 +0200

    copy by habrahabr.ru

---
+++ xor/src/xor.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Ольга on 18.11.2015.
+ */
+public class xor {
+    public static void main(String[] args) {
+
+    }
+
+    public static byte[] encode(String pText, String pKey) {
+        byte[] txt = pText.getBytes();
+        byte[] key = pKey.getBytes();
+        byte[] res = new byte[pText.length()];
+
+        for (int i = 0; i < txt.length; i++) {
+            res[i] = (byte) (txt[i] ^ key[i % key.length]);
+        }
+
+        return res;
+    }
+
+    public static String decode(byte[] pText, String pKey) {
+        byte[] res = new byte[pText.length];
+        byte[] key = pKey.getBytes();
+
+        for (int i = 0; i < pText.length; i++) {
+            res[i] = (byte) (pText[i] ^ key[i % key.length]);
+        }
+
+        return new String(res);
+    }
+}
