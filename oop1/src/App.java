d50a6d745c356e90aa1a4b39865a131b4a4a497e
Pavel <Pavel@test.com>
2015-11-20 20:57:16 +0200

    fgfd

--- oop1/src/App.java
+++ oop1/src/App.java
@@ -1,18 +1,5 @@
-/**
- * Created by ITHILLEL3 on 12.11.2015.
- */
-public class App {
-    public static void main(String[] args) {
-        //       Student ivanov = new Student("Ivan", "Ivanov");
-//        ivanov.name = "Ivan";
-//        ivanov.surname = "Ivanov";
+public class StudentsApp {
+        public static void main(String[] args) {
 
-        //ivanov.printInfo();
-
-        Student petrov = new Student("", "");
-//
-        petrov.printInfo();
-
-
-    }
+        }
 }
